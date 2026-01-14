5e40339e156f94300ba692153a8640599f329faa
Ashritagogula <ashritagogula12@gmail.com>
2026-01-14 20:43:54 +0530

    Update database configuration

---
+++ backend/src/config/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('connect', () => {
+  console.log('🗄️ Connected to PostgreSQL');
+});
+
+module.exports = pool;
