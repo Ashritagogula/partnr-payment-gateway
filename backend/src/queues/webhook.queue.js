7c8c182c18334b50f46d86d4d1d9082e8247d51f
Ashritagogula <ashritagogula12@gmail.com>
2026-01-14 20:48:45 +0530

    Update webhook queue

--- backend/src/queues/webhook.queue.js
+++ backend/src/queues/webhook.queue.js
@@ -1,5 +1,9 @@
 const Queue = require('bull');
+const deliverWebhookJob = require('../jobs/deliverWebhook.job');
 
 const webhookQueue = new Queue('webhooks', process.env.REDIS_URL);
+
+// 🔗 Connect queue to processor
+webhookQueue.process(deliverWebhookJob);
 
 module.exports = webhookQueue;
